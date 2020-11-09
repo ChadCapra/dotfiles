4b5f7446031963ada3641751e50337a8b2472de3
Chad Capra <ChadCapra@gmail.com>
2020-11-09 16:16:30 -0600

    add oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,8 @@ sudo apt update
 # Install vim, git, curl, wget, tmux, zsh
 sudo apt install -y vim git curl wget tmux zsh
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "The script works for now!"
+# end of script
+echo "You have reached the end of the setup script"
