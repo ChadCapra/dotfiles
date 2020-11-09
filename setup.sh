3292668a27a9fa38094cf64a5636b8819861ccab
Chad Capra <chadcapra@gmail.com>
2020-11-09 15:29:20 -0600

    adjust script to run installs

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Update apt for latest versions
 sudo apt update
 
-# Install vim, git, tmux, zsh, docker
-#sudo apt install -y vim git tmux zsh docker
+# Install vim, git, tmux, zsh
+sudo apt install -y vim git tmux zsh
 
 
 echo "The script works for now!"
