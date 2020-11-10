9b3ffb30e561e4a24146d59508aa6ce32f3f372e
Chad Capra <ChadCapra@gmail.com>
2020-11-09 18:45:47 -0600

    new test

--- setup.sh
+++ setup.sh
@@ -13,14 +13,17 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git config --global user.name "Chad Capra"
 git config --global user.email chadcapra@gmail.com
 
+echo "begin key gen"
 # create ssh key
 ssh-keygen -t ed25519 -C "chadcapra@gmail.com"
+
+echo "show key"
 
 # show public ssh-key to add to github
 cat ~/.ssh/id_ed25519.pub
 
 # pause to give user a chance to add
-read -p "Before continuing, please copy above key into github ssh keys, so we can download our configs!  Then... Press enter to continue"
+read -p "Press enter to continue"
 
 # end of script
 echo "You have reached the end of the setup script"
