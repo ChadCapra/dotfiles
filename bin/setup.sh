dbf72314a14dbba6457af4403ad9d58f71585de9
Chad Capra <chadcapra@gmail.com>
2020-11-11 16:44:18 -0600

    update setup file

--- bin/setup.sh
+++ bin/setup.sh
@@ -10,11 +10,15 @@ SSH_KEY_PATH="$HOME/.ssh/id_$SSH_KEY_TYPE"
 
 GIT_REPO_PATH="git@github.com:ChadCapra/dotfiles.git"
 GIT_LOCAL_DIR=dotfiles
+GIT_LOCAL_BAK=dotfiles-bak
 #           !!! IMPORTANT NOTE - PLEASE READ !!!
 #  GIT_LOCAL_DIR is the name of the folder where git repo is pulled into
 #  This path is used in other files for referencing location of git repo.
 #  Therefore, if you change the above variable, please change in the following files:
 #    - .zshrc
+
+# Move to home directory
+cd $HOME
 
 # Update apt for latest versions
 sudo apt update
@@ -58,21 +62,17 @@ git config --global user.email "$EMAIL"
 # clone git repo to "~/$GIT_LOCAL_DIR"
 git clone --bare $GIT_REPO_PATH $GIT_LOCAL_DIR
 
-# set alias
+# set alias and create backup folder (in case dotfiles already exist from installs)
 alias dotgit='/usr/bin/git --git-dir=$HOME/$GIT_LOCAL_DIR/ --work-tree=$HOME'
+mkdir $GIT_LOCAL_BAK
+mkdir $GIT_LOCAL_BAK/bin
 
 # checkout to home folder (to add/replace .vimrc, .zshrc, etc)
+dotgit checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} mv {} $GIT_LOCAL_BAK/{}
 dotgit checkout
-if [ $? = 0 ]; then
-  echo "Checked out $GIT_LOCAL_DIR.";
-else
-  echo "Backing up pre-existing dot files (before checking out to home directory).";
-  mkdir -p $GIT_LOCAL_DIR-backup
-  dotgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $GIT_LOCAL_DIR-backup/{}
-  dotgit checkout
-fi;
 
-#dotgit config status.showUntrackedFiles no
+# Set up stream for pushing updates back to github
+dotgit push --set-upstream origin main
 
 # end of script
 echo "Congrats on your newly setup machine!! Nice work :)"
