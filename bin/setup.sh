3a54b4fee3923ef46b96ff31f47335e6e0a51edc
Chad Capra <chadcapra@gmail.com>
2020-11-12 14:07:38 -0600

    allow multiple runs without overwrites

--- bin/setup.sh
+++ bin/setup.sh
@@ -10,7 +10,7 @@ SSH_KEY_PARAMS="-o -a 256"
 SSH_KEY_PATH="$HOME/.ssh/id_$SSH_KEY_TYPE"
 
 export DOTGIT_DIR=$HOME/dotfiles
-DOTGIT_BAK=$DOTGIT_DIR-bak
+DOTGIT_BAK=$DOTGIT_DIR-"$(date +"%Y%m%d_%H%M%S")"
 
 # Set current directory to home
 cd $HOME
@@ -25,22 +25,15 @@ sudo apt install -y vim git curl wget tmux zsh
 OH_MY_ZSH_URL=https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sh -c "$(curl -fsSL $OH_MY_ZSH_URL)"
 
-## Check if key exists at given ssh location (and ask user how to proceed)
-#if [ -f $SSH_KEY_PATH]; then
-#
-#  echo -n "ssh key already exists at $SSH_KEY_PATH"
-#  echo -n "Do you wish to (o)verwrite or (k)eep existing (o/k)?"
-#  read answer
-#  
-#  if [ "$answer" != "${answer#[Oo]}" ] ;then
-#
-#    # delete existing key (public & private)
-#    rm -f $SSH_KEY_PATH
-#    rm -f $SSH_KEY_PATH.pub
- 
+# config git
+git config --global user.name "$NAME"
+git config --global user.email "$EMAIL"
+
+# set alias
+alias dotgit='/usr/bin/git --git-dir=$DOTGIT_DIR/ --work-tree=$HOME'
+
 # create ssh key
 ssh-keygen $SSH_KEY_PARAMS -t $SSH_KEY_TYPE -C "$EMAIL" -f $SSH_KEY_PATH
-
 
 echo ""
 echo "###############################"
@@ -52,7 +45,8 @@ echo ""
 CONTENTS=sudo cat $SSH_KEY_PATH.pub
 echo "$CONTENTS" 
 
-# TODO: Automatically copy key into clipboard (once issue with char limit is figured out)
+# TODO: Automatically copy key into clipboard
+# (once issue with char limit is figured out)
 #printf "\033]52;c;$(echo "$CONTENTS" | base64)\a"
 
 echo "###############################"
@@ -61,27 +55,20 @@ echo "###############################"
 echo ""
 
 # pause to give user a chance to add key to github
-echo "Before continuing, please copy and paste ssh-key into github to allow access"
+echo "Before continuing, copy and paste ssh-key into github to allow access"
 echo ""
 read continue 
 
-# config git
-git config --global user.name "$NAME"
-git config --global user.email "$EMAIL"
-
-# remove previous git (and backup) folder
+# grab data from github and store in bare local dir: "~/$DOTGIT_DIR"
 sudo rm -rf $DOTGIT_DIR
-sudo rm -rf $DOTGIT_BAK
-
-# clone git repo to "~/$DOTGIT_DIR"
 git clone --bare $GIT_REPO_PATH $DOTGIT_DIR
 
-# set alias and create backup folder (in case dotfiles already exist from installs)
-alias dotgit='/usr/bin/git --git-dir=$DOTGIT_DIR/ --work-tree=$HOME'
+# create backup directory (with bin folder) to save existing files
 sudo mkdir -p $DOTGIT_BAK/bin
 
 # checkout to home folder (to add/replace .vimrc, .zshrc, etc)
-dotgit checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} sudo mv {} $DOTGIT_BAK/{}
+dotgit checkout 2>&1 | egrep "^\s+" | awk {'print $1'} \
+  | xargs -I{} mv {} $DOTGIT_BAK/{}
 dotgit checkout
 
 # Set up stream for pushing updates back to github
