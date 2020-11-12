d0b720c4ed234463355bcb8fbbada6b7a17950fc
Chad Capra <chadcapra@gmail.com>
2020-11-12 16:47:51 -0600

    remove sudo from mkdir

--- bin/setup.sh
+++ bin/setup.sh
@@ -58,7 +58,7 @@ read continue
 
 # delete existing bare git dir and create new backup folder
 rm -rf $DOTGIT_DIR
-sudo mkdir -p $DOTGIT_BAK/bin
+mkdir -p $DOTGIT_BAK/bin
 
 # grab data from github and store in bare local dir: "~/$DOTGIT_DIR"
 alias dotgit='/usr/bin/git --git-dir=$DOTGIT_DIR/ --work-tree=$HOME'
